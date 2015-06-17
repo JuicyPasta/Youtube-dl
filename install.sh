cc1aee2354df7c010c6b6b7f4fecec1b8136a617
Jackson Darrow <jackson.darrow@gmail.com>
2015-06-16 18:39:01 -0700

    downloads stuff and started installer

---
+++ install.sh
@@ -0,0 +1,20 @@
+if [ $OSTYPE = "darwin14" ]; then
+
+	if [ `node -v` != "v0.12.1" ]; then
+		echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+		. ~/.bashrc
+		mkdir ~/local
+		mkdir ~/node-latest-install
+		cd ~/node-latest-install
+		curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+		./configure --prefix=~/local
+		make install
+		curl https://www.npmjs.org/install.sh | sh
+	fi
+
+	git clone https://github.com/JuicyPasta/Youtube-dl ~/Youtube-dl
+	npm install ~/Youtube-dl
+	node ~/Youtube-dl start
+
+
+fi
