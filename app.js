66aae48118326be262bc999bf376069832f88e04
Jackson Darrow <jackson.darrow@gmail.com>
2015-06-24 15:34:32 -0700

    audio conversion works

--- app.js
+++ app.js
@@ -5,53 +5,68 @@ var ytdl = require('ytdl-core'); //https://www.npmjs.com/package/ytdl-core
 var startStopDaemon = require('start-stop-daemon'); //https://www.npmjs.com/package/start-stop-daemon
 var url = require('url');
 var path = require('path');
-var ffmetadata = require("ffmetadata");
-var Transcoder = require('stream-transcoder');
-var mp4 = require('mp4-parser');
 var ffmpeg = require('fluent-ffmpeg');
+var child_process = require('child_process');
 
 var httpsOptions = {
     key: fs.readFileSync('./tls/key.pem'),
     cert: fs.readFileSync('./tls/cert.pem')
 };
-var infoOptions = {
-    downloadURL: true
-};
-var downloadOptions = {
-    //filter: function(format) {
-        //return format.container == 'webm';
-    //},
-    filter: 'audioonly',
-    quality: 'highest'
-};
+
+
+
 
 var server = function (port) { https.createServer(httpsOptions, function (req, res) {
+    process.on('uncaughtException', function (err) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        return res.end("There was some type of error :/");
+    });
+
     if (req.url == '/ping'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         return res.end("Hello, world.");
     }
 
+
     var query = url.parse(req.url, true).query;
-    var info = url.parse(req.url, true);
 
     var destination = (query.destination) ? path.normalize(query.destination) : null,
         quality = query.quality,
-        filter = query.filter,
         format = query.format,
-        title = query.title,
-        link = query.link;
+        inputTitle = query.inputTitle,
+        link = query.link,
+        filterType = query.filterType;
 
+    var inputPass = true;
     if (!destination || !fs.existsSync(destination) || !fs.statSync(destination).isDirectory()){
-        res.writeHead(500, {'Content-Type': 'text/html'});
-        return res.end("Destination is invalid");
+        res.write("Destination is invalid\n");
+        inputPass = !inputPass;
     }
 
-    console.log();
+    if (['lowest', 'highest', '1080p', '720p', '480p', '360p', '240p', '144p'].indexOf(quality) < 0 && quality){
+        res.write("quality is invalid\n");
+        inputPass = !inputPass;
+    }
+    if (['mp3', 'mp4'].indexOf(filterType) < 0 && filterType){
+        res.write("filter is invalid\n");
+        inputPass = !inputPass;
+    }
+    if (!inputPass){
+        return res.end('\n');
+    }
+
+    var infoOptions = {
+        downloadURL: true
+    };
+    var downloadOptions = {
+        quality: quality || 'highest',
+    };
+
+    console.log('____________________________INPUT INFORMATION____________________________');
     console.log("destination: " + destination);
     console.log("quality: " + quality);
-    console.log("filter: " + filter);
     console.log("format: " + format);
-    console.log("title: " + title);
+    console.log("inputTitle: " + inputTitle);
     console.log("link: " + link);
 
     ytdl.getInfo(link, infoOptions, function(err, info){
@@ -61,49 +76,48 @@ var server = function (port) { https.createServer(httpsOptions, function (req, r
             return res.end("This url is not valid");
         }
 
-        title = title || info.title;
-        var location = destination;
+        var title = inputTitle || info.title;
         var thumbnailurl = info.thumbnail_url;
+        // var extension = "";
+        // if (info.container) extension = '.' + info.container;
+        var location;
+        if (filterType == 'mp3'){
+            location = path.join(destination, title) + '.mp3';
+        }else{
+            location = path.join(destination, title) + '.mp4';
+        }
 
-        // console.log();
-        // console.log("title: " + title);
-        // console.log("location: " + location);
+        console.log('____________________________VIDEO INFORMATION____________________________');
+        console.log("title: " + title);
         // console.log("extension: " + extension);
-        // console.log("thumbnailurl: " + thumbnailurl);
-
+        console.log("thumbnailurl: " + thumbnailurl);
 
         ytdl.downloadFromInfo(info, downloadOptions)
             .on('format', function(format){ // ytdl has decided on a format, lets start piping it and convert all of the data
 
-                this
-                    .on('end', function(){
+                // var output_path = path.join(location, title) + '.mp4';
+                // var output_stream = fs.createWriteStream(path.join(location, title) + '.mp4');
+
+                console.log(location);
+
+                var converter = child_process.spawn('ffmpeg', ['-i', 'pipe:0', '-q:a', '0', '-map', 'a', location]);
 
 
-                        // // Read song.mp3 metadata
-                        // ffmetadata.read(path.join(location, title) + '.mp4', function(err, data) {
-                        //     if (err) console.error("Error reading metadata", err);
-                        //     else console.log(data);
-                        // });
 
-                        // // Set the artist for song.mp3
-                        // var data = {
-                        //     //artist: "Me",
-                        //     // duration: 10
-                        // };
-                        // var options = {
-                        //     attachments: ['cover']
-                        // };
-                        // ffmetadata.write(path.join(location, title) + '.mp4', data, function(err) {
-                        //     if (err) console.error("Error writing metadata", err);
-                        //     else console.log("Data written");
-                        // });
+                converter.stderr.pipe(process.stdout);
 
-
+                this.on('end', function() {
+                    console.log('Finished processing');
+                    // fs.rename(location + '.part', location, function(err){
                         res.writeHead(200, {'Content-Type': 'text/html'});
                         res.end("video successfully downloaded");
-                    })
+                    // });
+                })
+                .on('error', function(err){
+                    console.log(err);
+                })
+                .pipe(converter.stdin);
 
-                    .pipe(fs.createWriteStream(path.join(location, title) + '.mp4'));
 
 
             })
@@ -114,34 +128,9 @@ var server = function (port) { https.createServer(httpsOptions, function (req, r
             })
             .pause();
 
-
     });
 
 }).listen(port);};
 
 server(6299);
 //startStopDaemon(server(6299));
-
-
-//.on('metadata', function(data){
-//     console.log(data.input.streams);
-// })
-// .on('error', function(err){
-//     console.log(err)
-// })
-// .on('finish', function(){
-//     console.log('done!')
-// })
-
-// .maxSize(320, 240)
-// .videoCodec('h264')
-// .videoBitrate(800 * 1000)
-// .fps(25)
-
-// //.audioCodec('flac')
-// .sampleRate(44100)
-// .channels(2)
-// .audioBitrate(128 * 1000)
-// .format('mp4')
-// .stream()
-// .pipe(fs.createWriteStream(path.join(location, title) + '.mp4'));
