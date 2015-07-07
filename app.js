c73427fa7d76ca7abb0fbbbbc1d33ba73fdae6c9
Jackson Darrow <jackson.darrow@gmail.com>
2015-07-07 01:58:02 -0700

    mixtapes copped

--- app.js
+++ app.js
@@ -16,25 +16,22 @@ var httpsOptions = {
 var server = function (port) {
 
     https.createServer(httpsOptions, function (req, res) {
-    process.on('uncaughtException', function (err) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        return res.end("There was some type of error :/");
-    });
+    // process.on('uncaughtException', function (err) {
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     return res.end("There was some type of error :/");
+    // });
 
     if (req.url == '/ping'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         return res.end("Hello, world.");
     }
 
-
     var query = url.parse(req.url, true).query;
 
     var destination = (query.destination) ? path.normalize(query.destination) : null,
-        quality = query.quality,
-        format = query.format,
+        // quality = query.quality,
         inputTitle = query.inputTitle,
-        link = query.link,
-        filterType = query.filterType;
+        link = query.link;
 
     var inputPass = true;
     if (!destination || !fs.existsSync(destination) || !fs.statSync(destination).isDirectory()){
@@ -42,14 +39,11 @@ var server = function (port) {
         inputPass = !inputPass;
     }
 
-    if (['lowest', 'highest', '1080', '720', '480', '360', '240', '144'].indexOf(quality) < 0 && quality){
-        res.write("quality is invalid\n");
-        inputPass = !inputPass;
-    }
-    if (['mp3', 'mp4'].indexOf(filterType) < 0 && filterType){
-        res.write("filter is invalid\n");
-        inputPass = !inputPass;
-    }
+    // if (['lowest', 'highest', '1080', '720', '480', '360', '240', '144'].indexOf(quality) < 0 && quality){
+    //     res.write("quality is invalid\n");
+    //     inputPass = !inputPass;
+    // }
+
     if (!inputPass){
         return res.end('\n');
     }
@@ -57,25 +51,17 @@ var server = function (port) {
     var infoOptions = {
         downloadURL: true
     };
+
     var downloadOptions = {
-        quality: quality || 'highest',
+        // quality: quality || 'highest',
     };
 
-    console.log('____________________________INPUT INFORMATION____________________________');
-    console.log("destination: " + destination);
-    console.log("quality: " + quality);
-    console.log("format: " + format);
-    console.log("inputTitle: " + inputTitle);
-    console.log("link: " + link);
+    // console.log('____________________________INPUT INFORMATION____________________________');
+    // console.log("destination: " + destination);
+    // console.log("quality: " + quality);
+    // console.log("inputTitle: " + inputTitle);
+    // console.log("link: " + link);
 
-    downloadSong(req, link, infoOptions);
-
-
-}).listen(port);};
-
-server(6299);
-
-function downloadSong (res, link, infoOptions){
     ytdl.getInfo(link, infoOptions, function(err, info){
         if (err){
             console.log('there was an error');
@@ -85,41 +71,24 @@ function downloadSong (res, link, infoOptions){
 
         var title = inputTitle || info.title;
         var thumbnailurl = info.thumbnail_url;
-        // var extension = "";
-        // if (info.container) extension = '.' + info.container;
-        var location;
-        if (filterType == 'mp3'){
-            location = path.join(destination, title) + '.mp3';
-        }else{
-            location = path.join(destination, title) + '.mp4';
-        }
 
-        console.log('____________________________VIDEO INFORMATION____________________________');
-        console.log("title: " + title);
-        // console.log("extension: " + extension);
-        console.log("thumbnailurl: " + thumbnailurl);
+        var location = path.join(destination, title) + '.mp3';
 
         ytdl.downloadFromInfo(info, downloadOptions)
             .on('format', function(format){ // ytdl has decided on a format, lets start piping it and convert all of the data
 
-                console.log(location);
-
-
                 var args = ['-i', 'pipe:0', '-q:a', '0', '-map', 'a'];
-                //args.concat(['-metadata', 'artist=\"lmfao\"']);
-                //args.concat(['-f', filterType]);
-                args.push(location);
+                // args.concat(['-metadata', 'artist=\"lmfao\"']);
                 // args.concat(['-f', filterType]);
+                args.push(location);
                 var converter = child_process.spawn('ffmpeg', args);
 
                 converter.stderr.pipe(process.stdout);
 
                 this.on('end', function() {
                     console.log('Finished processing');
-                    fs.rename(location + '.part', location, function(err){
                         res.writeHead(200, {'Content-Type': 'text/html'});
                         res.end("video successfully downloaded");
-                    });
                 })
 
                 .on('error', function(err){
@@ -135,7 +104,11 @@ function downloadSong (res, link, infoOptions){
             })
             .pause();
     });
-}
+
+}).listen(port);};
+
+
+server(6299);
 
 // startStopDaemon({
 //     outFile: '/Users/jackson/Desktop/out.txt',
